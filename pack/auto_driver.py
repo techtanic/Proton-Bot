7c8d54fa3a1b8c32d03a48ddb7e31d8909fe997a
TECHTANIC <64263079+techtanic@users.noreply.github.com>
2021-02-21 18:28:55 +0300

    more fix

--- pack/auto_driver.py
+++ pack/auto_driver.py
@@ -2,20 +2,8 @@ import requests
 import wget
 import zipfile
 import os
-import platform
 
-def autodriver():
-
-    os_name = platform.system()
-    if os_name == "Linux":
-        arch = 'linux64'
-
-    if os_name == "Windows":
-        arch = 'win32'
-        
-    if os_name == "Darwin":
-        arch = 'mac64'
-        
+def autodriver(arch):   
     url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
     response = requests.get(url)
     version_number = response.text
@@ -25,5 +13,9 @@ def autodriver():
     latest_driver_zip = wget.download(download_url,'chromedriver.zip')
 
     with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
-        zip_ref.extractall()
+        try:
+            zip_ref.extractall()
+        except PermissionError:
+            return False
     os.remove(latest_driver_zip)
+    return True
--- proton.py
+++ proton.py
@@ -12,33 +12,38 @@ init(convert = True)
 
 driver_path = "default"
 
-print("Downloading Chrome Driver")
-autodriver()
-
 os_name = platform.system()
 if os_name == "Linux":
     clear_cmd = 'clear'
+    arch = 'linux64'
 
 if os_name == "Windows":
     clear_cmd = 'cls'
-    
+    arch = 'win32'
+
 if os_name == "Darwin":
     clear_cmd = 'clear'
-    
+    arch = 'mac64'
 
 def clear():
     return os.system(clear_cmd)
 
-clear()
+if autodriver():
 
-options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument("--ignore-certificate-error")
-options.add_argument("--ignore-ssl-errors")
-options.add_argument('--log-level=3')
-driver = webdriver.Chrome(options = options)
-print("- . -.-. .... - .- -. .. -.-.")
+    clear()
+
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("--ignore-certificate-error")
+    options.add_argument("--ignore-ssl-errors")
+    options.add_argument('--log-level=3')
+    driver = webdriver.Chrome(options = options)
+    print("- . -.-. .... - .- -. .. -.-.")
 
 
-x_i, y_i = calculate_move()
-create_account(driver, x_i, y_i)
+    x_i, y_i = calculate_move()
+    create_account(driver, x_i, y_i)
+
+else:
+    print("Could not complete, because the old driver is in use.")
+
